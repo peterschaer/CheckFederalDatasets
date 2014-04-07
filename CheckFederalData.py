c906c1fe5804a1e0195f7319960ad9911f5e6611
Peter Schaer <peter.schaer@bve.be.ch>
2014-04-07 08:56:30 +0200

    Readme.txt werden heruntergeladen

--- CheckFederalData.py
+++ CheckFederalData.py
@@ -1,10 +1,34 @@
 import urllib2
 from bs4 import BeautifulSoup
+import tempfile
+import os
 
 federalDataURL = "http://data.geo.admin.ch/"
+tempDir = tempfile.mkdtemp()
+print tempDir
 
-f = urllib2.urlopen(federalDataURL)
-soup = BeautifulSoup(f)
+def getTopicList():
+	f = urllib2.urlopen(federalDataURL)
+	soup = BeautifulSoup(f)
 
-dataDiv = soup.find_all("div", class_="data")
-print dataDiv
+	dataDiv = soup.find_all("div", class_="data")[0]
+	#~ print dataDiv
+
+	federalTopics = []
+
+	links = dataDiv.find_all("a")
+	for link in links:
+		federalTopics.append(link.get_text())
+		
+	return federalTopics
+	
+topics = getTopicList()
+for t in topics:
+	topicURL = federalDataURL + (t)
+	topicURLReadme = topicURL + "/readme.txt"
+	print topicURL
+	tempFile = os.path.join(tempDir, t + "_readme.txt")
+	f = urllib2.urlopen(topicURLReadme)
+	with open(tempFile, "wb") as code:
+		code.write(f.read())
+	
