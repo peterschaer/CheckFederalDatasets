8030b4cd648fe4228643c8a7d2ddc4669f0ee544
Peter Schaer <peter.schaer@bve.be.ch>
2014-04-08 11:22:44 +0200

    MD5-Vergleich eingebaut; SQLite-DB mit alten MD5 eingelesen

--- CheckFederalDatasets.py
+++ CheckFederalDatasets.py
@@ -1,3 +1,4 @@
+# -*- coding: latin-1 -*-
 import urllib2
 from bs4 import BeautifulSoup
 import tempfile
@@ -5,47 +6,21 @@ import os
 import FederalTopic
 
 federalDataURL = "http://data.geo.admin.ch/"
-#~ tempDir = tempfile.mkdtemp()
-#~ print "Temp-Dir: " + tempDir
 
 def getTopicList():
+	topics = []
+		
 	f = urllib2.urlopen(federalDataURL)
 	soup = BeautifulSoup(f)
-
 	dataDiv = soup.find_all("div", class_="data")[0]
-	#~ print dataDiv
-
-	topics = []
-
 	links = dataDiv.find_all("a")
+	
 	for link in links:
 		topics.append(link.get_text())
 		
 	return topics
 
-def downloadReadme(topics):
-	readmes = []
-	for t in topics:
-		topicURL = federalDataURL + (t)
-		topicURLReadme = topicURL + "/readme.txt"
-		tempFile = os.path.join(tempDir, t + "_readme.txt")
-		f = urllib2.urlopen(topicURLReadme)
-		with open(tempFile, "wb") as code:
-			code.write(f.read())
-		readmes.append(tempFile)
-	return readmes
-
-#~ def extractMD5(readme):
-	#~ print readme
-
 federalTopics = getTopicList()
 for f in federalTopics:
 	ft = FederalTopic.FederalTopic(f, federalDataURL + f)
-	print ft.tempFile
-	
-#~ federalTopicsReadme = downloadReadme(federalTopics)
-
-#~ for f in federalTopicsReadme:
-	#~ md5 = extractMD5(f)
-
-#~ TODO: delete Tempdir
+	print f + "," + ft.newMD5 + "  " + ft.oldMD5 + " Status: " + ft.status
--- FederalTopic.py
+++ FederalTopic.py
@@ -1,33 +1,66 @@
+# -*- coding: latin-1 -*-
 import urllib2
 import tempfile
 import os
 import shutil
+import hashlib
+import sqlite3
 
 class FederalTopic():
 	def __init__(self, name, URL):
 		self.name = name
 		self.url = URL
-		self.tempFile = ""
+		self.ReadmeFile = ""
 		self.tempDir = ""
+		self.ReadmeContent =""
 		self.oldMD5 = ""
 		self.newMD5 = ""
+		self.configDB = r"C:\Daten\Repos\CheckFederalDatasets\federalTopicsConf.sqlite"
+		self.status = "unassigned"
 
 		self.__downloadReadme__()
+		self.__getMD5FromReadme_()
 		self.__deleteTempDir__()
+		self.__getMD5FromDB__()
+		self.__compareMD5__()
 		
 	def __downloadReadme__(self):
 		topicURLReadme = self.url + "/readme.txt"
 		self.tempDir = tempfile.mkdtemp()
-		self.tempFile = os.path.join(self.tempDir, self.name + "_readme.txt")
+		self.ReadmeFile = os.path.join(self.tempDir, self.name + "_readme.txt")
 		f = urllib2.urlopen(topicURLReadme)
-		with open(self.tempFile, "wb") as code:
+		with open(self.ReadmeFile, "wb") as code:
 			code.write(f.read())
 		
-	def __extractMD5Checksum__(self):
-		readmeFile = open(self.tempFile, "r")
-		
+	def __getFileContent__(self):
+		readmeFile = open(self.ReadmeFile, "r")
+		self.ReadmeContent = readmeFile.read()
 		readmeFile.close()
+		
+	def __getMD5FromReadme_(self):
+		self.__getFileContent__()
+		searchString = "MD5Checksum:"
+		checkSumLength = 32
+		text = self.ReadmeContent
+		index = text.index(searchString) + len(searchString)
+		#~ Es müssen genau 32 Zeichen für die Checksumme aus dem String extrahiert werden
+		#~ Einige Readmes haben am Schluss noch Zeilenumbrüche
+		self.newMD5 = text[index:index+checkSumLength]
 		
 	def __deleteTempDir__(self):
 		shutil.rmtree(self.tempDir)
+		
+	def __getMD5FromDB__(self):
+		connection = sqlite3.connect(self.configDB)
+		sql = "SELECT md5 from federalTopics WHERE topic='" + self.name + "'"
+		cursor = connection.cursor()
+		self.oldMD5 = cursor.execute(sql).fetchone()[0]
+		cursor.close()
+		connection.close()
+		
+	def __compareMD5__(self):
+		if self.oldMD5 == self.newMD5:
+			self.status = "unchanged"
+		else:
+			self.status = "changed"
 		
